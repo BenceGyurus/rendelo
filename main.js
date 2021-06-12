36634aa2dafe4c970502ea878c5ec941f7d9a78c
BenceGyurus <81524938+BenceGyurus@users.noreply.github.com>
2021-06-12 22:48:45 +0200

    Add

---
+++ main.js
@@ -0,0 +1,120 @@
+
+data_List = ["", "null", ""]; //name - soup  - menu
+errors = [];
+number_Of_Menu = "0";
+function query_Data(url,method, callback){
+    data = "";
+   var req = new XMLHttpRequest();
+   req.onreadystatechange = function(){
+       if (this.readyState == 4 && this.status == 200){
+           callback(this.responseText);
+       }
+   };
+   req.open(method, url);
+   req.send();
+   console.log(data);
+   return data;
+}
+
+function element_Menu(data){
+    if (data == ""){
+        data = "<h3>Ez a menü üres</h3>";
+        window.errors.push("Egy menü tartalma üres, előfordul, hogy még nem töltték ki.")
+    }
+    document.getElementById(id).innerHTML = data;
+    document_Error_Div();
+}
+
+function choose_Menu(number_Of_Menu){
+    for (i = 1; i < 4; i++){
+        b = String(i);
+            id = "menu"+b;
+            document.getElementById(id).style.border = "3px solid black"
+    }
+    id = "menu"+number_Of_Menu;
+    document.getElementById(id).style.border = "3px solid green"
+    window.data_List[2] = number_Of_Menu;
+}
+
+function looking_Menu(number_Of_Menu){
+    window.number_Of_Menu = number_Of_Menu;
+    id = "diplay_Menu"+number_Of_Menu;
+    if (document.getElementById(id).style.display != "block"){
+        document.getElementById(id).style.display = "block"
+    query_Data("menu"+number_Of_Menu+".html", "GET", element_Menu);
+    }
+    else{
+        document.getElementById(id).style.display = "none";
+    }
+}
+
+function loadSetting(){
+    console.log(window.innerWidth);
+    if (window.innerWidth > 800){
+        document.getElementById("menu").style.width = "50%";
+    }
+}
+
+loadSetting();
+function soup(a){
+    document.getElementById("yes").style.border = "3px solid #9c440e";
+    document.getElementById("no").style.border = "3px solid #9c440e";
+    document.getElementById("yes").style.color = "#4d4b4b";
+    document.getElementById("no").style.color = "#4d4b4b";
+    if (a){
+        document.getElementById("yes").style.border = "3px solid green";
+        document.getElementById("yes").style.color = "green";
+    }
+    else{
+        document.getElementById("no").style.border = "3px solid red";
+        document.getElementById("no").style.color = "red";
+    }
+    window.data_List[1] = a;
+}
+
+function document_Error_Div(){
+    console.log(window.errors);
+    document.getElementById("error_Div").innerHTML = "";
+    for (var i = 0; i < window.errors.length; i++){
+        document.getElementById("error_Div").innerHTML += window.errors[i]+"<br />"
+    }
+    window.errors = [];
+}
+
+function control_Datas(){
+    console.log("fut")
+    if (document.getElementById("name").value.length > 3 && window.data_List[1] != "null" && window.data_List[2] != ""){
+        window.data_List[0] = document.getElementById("name").value;
+        send();
+    }
+    else{
+        if (document.getElementById("name").value.length <= 3){
+            window.errors.push("A név minimális hossza 4 karakter!");
+        }
+        if (window.data_List[1] == "null"){
+            window.errors.push("Kérem válasszon levest!");
+        }
+        if (window.data_List[2] == ""){
+            window.errors.push("Kérem válasszon menüt!");
+        }
+        console.log(window.errors);
+        document_Error_Div();
+    }
+}
+
+function send(){
+    var req = new XMLHttpRequest();
+   req.onreadystatechange = function(){
+       if (this.readyState == 4 && this.status == 200){
+            document.body.innerHTML = this.responseText;
+       }
+   };
+   req.open("POST", "rendeles.html");
+req.setRequestHeader("content-type", "application/json");
+   var dic = JSON.stringify({  name: window.data_List[0],
+            soup: window.data_List[1],
+            menu: window.data_List[2]
+    });
+    console.log(dic);
+    req.send(dic);
+}
---
+++ server.js
@@ -0,0 +1,84 @@
+
+const fs = require("fs");
+const http = require("http");
+var count = 0;
+const requestListener = function(req, res) {
+    path = req.url;
+    if (req.method == "GET"){
+    if (path == "/"){
+        path = "/index.html"
+    }
+    extension = "html";
+    response = 404;
+    path = path.split("?")[0];
+    console.log(path);
+    extension = path.split(".")[path.split(".").length-1];
+    switch(true){
+        case extension == "html":
+            res.setHeader("content-type", "text/html");break;
+        case extension == "css":
+            res.setHeader("content-type", "text/css"); break;
+        case extension == "js":
+            res.setHeader("content-type", "text/js"); break;
+        case extension == "png" || extension == "svg" || extension == "img":
+            res.setHeader("Content-text", "image/"+extension); break;
+    }
+    fs.readFile(__dirname + path, function(err, data) {
+        if (err){
+            res.writeHead(404);
+            fs.readFile(__dirname + "/error.html", function(err, error_Data){
+                res.end(error_Data);
+            })
+        }
+        else{
+            res.writeHead(200);
+            res.end(data);
+        }
+    });
+}
+else if(req.method == "POST"){
+    var body = "";
+    req.on('data', function (chunk) {
+        body += chunk;
+      });
+      req.on('end', function () {
+        //var body = JSON.stringify(body);
+        var fs = require('fs');
+        console.log(body["name"]);
+        fs.writeFile(count+".json", body, function(err, result) {
+        if(err) console.log('error', err);
+        });
+        count++;
+      });
+      extension = "html";
+      response = 404;
+      path = path.split("?")[0];
+      console.log(path);
+      extension = path.split(".")[path.split(".").length-1];
+      switch(true){
+          case extension == "html":
+              res.setHeader("content-type", "text/html");break;
+          case extension == "css":
+              res.setHeader("content-type", "text/css"); break;
+          case extension == "js":
+              res.setHeader("content-type", "text/js"); break;
+          case extension == "png" || extension == "svg" || extension == "img":
+              res.setHeader("Content-text", "image/"+extension); break;
+      }
+      fs.readFile(__dirname + path, function(err, data) {
+          if (err){
+              res.writeHead(404);
+              fs.readFile(__dirname + "/error.html", function(err, error_Data){
+                  res.end(error_Data);
+              })
+          }
+          else{
+              res.writeHead(200);
+              res.end(data);
+          }
+      });
+}
+    
+}
+const server = http.createServer(requestListener);
+server.listen(8000, '192.168.1.4');
